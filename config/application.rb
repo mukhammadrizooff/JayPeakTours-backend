4c3490ecfb1b94ec958767586ed3596e331d08b9
mukhammadrizooff <legend.it@bk.ru>
2022-06-22 03:37:03 +0500

    Modify app and config components

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,25 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module VermontBackEnd1
+module VacayhomeBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post options]
+      end
+    end
+
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +28,10 @@ module VermontBackEnd1
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
