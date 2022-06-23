fd9676ef3b1ecf2f558a4717ec2ffff963c9e119
Sumair <sumairqaisar992@gmail.com>
2022-06-23 20:52:18 +0500

    Fix linter errors

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,3 @@
-class hotel < ApplicationRecord
+class Hotel < ApplicationRecord
   has_many :bookings
 end
--- test/models/hotel_test.rb
+++ test/models/hotel_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class hotelTest < ActiveSupport::TestCase
+class HotelTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
