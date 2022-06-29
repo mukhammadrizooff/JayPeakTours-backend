90ce938e464205ec63ce1d1215940f0d45e5df4d
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-29 10:37:05 -0500

    Add Tours controller actions.

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -1,9 +1,37 @@
 class Api::V1::ToursController < ApplicationController
-  def index; end
+  def index
+    tours = Tour.all.order(price: :asc)
+    render json: tours
+  end
 
-  def show; end
+  def show
+    tour = Tour.find(params[:id])
+    render json: tour
+  end
 
-  def create; end
+  def create
+    tour = Tour.new({
+                      name: params[:name],
+                      description: params[:description],
+                      duration: params[:duration],
+                      capacity: params[:capacity],
+                      guides: params[:guides],
+                      image_url: params[:image_url]
+                    })
 
-  def destroy; end
+    if tour.save
+      render json: tour
+    else
+      render json: { response: 'Error creating the tour item.' }
+    end
+  end
+
+  def destroy
+    tour = Tour.find(params[:id])
+    if tour.destroy
+      render json: { response: 'Tour item deleted successfully' }
+    else
+      render json: { response: 'Error deleting tour item' }
+    end
+  end
 end
