11919c6bc91513f79688467fff5e11deca1f309c
mukhammadrizooff <legend.it@bk.ru>
2022-06-29 21:18:15 +0500

    Add  parametres for create and index booking controller

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+    render json: @bookings
+  end
+  
   def create
     @booking = Booking.new(booking_params)
     return render json: @booking.errors, status: :unprocessable_entity unless @booking.save
 
     render json: @booking, status: :created
-  end
-
-  def index
-    @bookings = Booking.all
-    render json: @bookings
   end
 end
