a68ba3d342ed4279d1bc10900216daf1cdfb697f
Sumair <sumairqaisar992@gmail.com>
2022-07-01 12:33:51 +0500

    Add index route and method in user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tours, only: %i[index show create destroy]
-      resources :users, only: %i[show create]
-      resources :bookings, only: %i[show create]
+      resources :users, only: %i[index show create]
+      resources :bookings, only: %i[index create]
     end
   end
 
